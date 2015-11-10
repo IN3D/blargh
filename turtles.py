2349022a23fc9ba277c1d50ecd51e62738a2d630
Eric Hopkins <eric.on.tech@gmail.com>
2015-11-10 16:42:17 -0500

    Refactor turtles.py

--- turtles.py
+++ turtles.py
@@ -1,11 +1,13 @@
-turtles_we_like = [
-    'Painted Turtle',
-    'African Slideneck',
-    'Red Eared Slider',
-    'Razor Shell'
+turtles = [
+    'Painted Turtle', 'African Slideneck',
+    'Red Eared Slider', 'Razor Shell',
+    'Box Turtle', "Reeve's Turtle",
+    'Wood Turtle', 'Russian Tortise'
     ]
 
 print "Let's talk about the turtles we like..."
+print 'Turtles are a Testutadae (sp?)'
+print "isn't that neat-o???"
 
-for turtle in turtles_we_like:
-  print 'I like ' + turtle + 's'
+for turtle in turtles:
+  print 'I like ' + turtle + 's A LOT'
