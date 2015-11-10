d46bc6277133f9af360b6a3e4a848898addf629f
Eric Hopkins <eric.on.tech@gmail.com>
2015-11-10 16:44:01 -0500

    Refactor moon turtles

--- turtles.lua
+++ turtles.lua
@@ -1,5 +1,11 @@
-turtles = ['red eard slider', 'african slideneck']
+turtles = [
+  'Red eared slider',
+  'African Slideneck'
+  'Box Turtle'
+  'Wood Turtle'
+]
 
+print("Pure SCIENCE! fact: Turtles love the moon")
 for turtle in list_iter(turtles) do
   print(turtle)
 end
